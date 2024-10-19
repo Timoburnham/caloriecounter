966d9f5ebd968f45732054a0cb946d7a2ebb9aa7
Timo Burnham <timoburnham@gmail.com>
2024-10-19 11:03:11 -0500

    add message border

--- Calcounter/main.py
+++ Calcounter/main.py
@@ -2,10 +2,17 @@ from tkinter import *
 
 
 main_window= Tk()
-main_window.geometry("500x500")
+main_window.geometry("1000x1000")
 main_window.title("Calorie Counter")
 
-label = Label(main_window, text="Meeting your physique goals is easy with this calorie tracker.", font=('arial', 10, 'bold'))
+label = Label(main_window, text="Meeting your physique goals is easy with this calorie tracker.",
+ font=('arial', 10, 'bold'),
+ fg='#FFFFFF',
+ bg='black',
+ relief=RAISED,
+ bd=5,
+ padx=15
+ )
 label.pack()
 
 main_window.mainloop()
