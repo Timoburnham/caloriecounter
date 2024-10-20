030ac0d7dd07009918f79b1f024abe7d9ad650fd
Timo Burnham <timoburnham@gmail.com>
2024-10-20 18:22:12 -0500

    added foods and logic to collect calorie ammount

--- Calcounter/main.py
+++ Calcounter/main.py
@@ -1,18 +1,59 @@
-from tkinter import *
+
+import customtkinter
+
+#app logic
+
+class Food:
+    def __init__(self, name, calories, protein, carbs, fat):
+        self.name = name
+        self.calories = calories
+        self.protein = protein
+        self.carbs = carbs
+        self.fat = fat
+
+food_list = [
+    Food("Apple", 95, 0.5, 25, 0.3),
+    Food("Chicken Breast", 165, 31, 0, 3.6),
+    Food("Brown Rice", 215, 5, 45, 1.5),
+]
 
 
-main_window= Tk()
-main_window.geometry("1000x1000")
-main_window.title("Calorie Counter")
 
-label = Label(main_window, text="Meeting your physique goals is easy with this calorie tracker.",
- font=('arial', 10, 'bold'),
- fg='#FFFFFF',
- bg='black',
- relief=RAISED,
- bd=5,
- padx=15
- )
-label.pack()
 
-main_window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+#create window
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+root = customtkinter.CTk()
+root.geometry("500x350")
+
+def login():
+    print("test")
+
+
+
+food_entry = customtkinter.CTkEntry(root, width=200, placeholder_text="Enter food item")
+food_entry.pack(pady=10)
+
+
+
+
+
+
+
+
+root.mainloop()
